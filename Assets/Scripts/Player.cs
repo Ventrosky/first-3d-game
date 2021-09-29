e7383ab84523e2a02e07fa8258b6d92415abbe9a
Salvatore Ventrone <ventrosky@gmail.com>
2021-09-29 22:25:58 +0200

    Player Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour{
+    public float moveSpeed;
+    public Rigidbody rig;
+
+    // Update is called once per frame
+    void Update(){
+        float x = Input.GetAxis("Horizontal") * moveSpeed;
+        float z = Input.GetAxis("Vertical") * moveSpeed;
+
+        rig.velocity = new Vector3(x, rig.velocity.y, z);
+
+        Vector3 vel = rig.velocity;
+        vel.y = 0;
+
+        if (vel.x != 0 || vel.y != 0) {
+            transform.forward = vel;
+        }
+    }
+}
